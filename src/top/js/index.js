d73eaf008cda485d3a0cc7d1de8e1dc02746d3d8
fukugit <fukuma1234@gmail.com>
2021-06-24 23:26:55 +0900

    Add some content in left contenor

--- src/top/js/index.js
+++ src/top/js/index.js
@@ -1,10 +1,11 @@
 import '../css/style.scss';
 
-const red = document.getElementById('box-introduction');
+const introduction = document.getElementById('box-introduction');
+const hugo = document.getElementById('box-hugo');
+const sns = document.getElementById('box-sns');
 window.addEventListener('scroll', function () {
   const value = window.scrollY;
-  // red.style.top = value * 0.5 + 'px';
-  red.style.left = value * 0.3 + 'px';
-  // red.style.width = value * 1 + 'px';
-  // red.style.height = value * 1 + 'px';
+  introduction.style.left = value * 0.3 + 'px';
+  hugo.style.left = (value - 3500) * 0.3 + 'px';
+  sns.style.left = (value - 3500 * 2) * 0.3 + 'px';
 });
