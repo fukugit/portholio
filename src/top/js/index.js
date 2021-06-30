978c0d13249b56dd6918a091d859dada2a3509cb
fukugit <fukuma1234@gmail.com>
2021-06-30 23:20:13 +0900

    Add right contents

--- src/top/js/index.js
+++ src/top/js/index.js
@@ -1,9 +1,7 @@
 import '../css/style.scss';
 
-// SASS 内で url()で画像を利用する場合はここに定義する必要があるので注意。
-// なぜ必要かは不明...
 // In case of using url() in sass file, this import steatment is necessary.  
-// But it is no idea why it is neccesry.  
+// But I have no idea why it is neccesry to use image file in sass.  
 import '../img/balloon.png'
 import '../img/cloud.png'
 
@@ -11,8 +9,14 @@ const introduction = document.getElementById('box-introduction');
 const hugo = document.getElementById('box-hugo');
 const sns = document.getElementById('box-sns');
 window.addEventListener('scroll', function () {
+  scrollRotate('cloud');
   const value = window.scrollY;
   introduction.style.left = value * 0.2 + 'px';
   hugo.style.left = (value - 3500) * 0.3 + 'px';
   sns.style.left = (value - 3500 * 2) * 0.3 + 'px';
 });
+
+function scrollRotate(id) {
+  let image = document.getElementById(id);
+  image.style.transform = "rotate(" + window.pageYOffset/2 + "deg)";
+}
